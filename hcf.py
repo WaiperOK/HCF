735bd41f783735b40226af122a9dd635838555f8
WaiperOK <86571941+WaiperOK@users.noreply.github.com>
2021-12-30 14:53:15 +0200

    Add files via upload

---
+++ hcf.py
@@ -0,0 +1,10 @@
+# Функция поиска HCF
+#Использование алгоритма Евклида
+
+def compute_hcf(x, y):
+    while (y):
+        x,y = y, x % y
+    return x
+
+hcf = compute_hcf(300,400)
+print ("The HCF is", hcf)
